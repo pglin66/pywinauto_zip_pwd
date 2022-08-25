61970297d0079babcac6bc8d36d8ffae6ee549ea
plin <pglin66@126.com>
2022-08-25 13:44:12 +0800

    xiugai

--- aaa.py
+++ aaa.py
@@ -2,7 +2,7 @@ import time
 import threading
 from pywinauto.application import Application
 from pywinauto.keyboard import send_keys
-app = Application(backend='uia').start("C:\Program Files (x86)\Elcomsoft Password Recovery\Advanced Archive Password Recovery\ARCHPR.exe /dontstart G:\JGB\山东\山东_SCJ_2022_02月 - 副本.zip")
+app = Application(backend='uia').start("C:\Program Files (x86)\Elcomsoft Password Recovery\Advanced Archive Password Recovery\ARCHPR.exe /dontstart F:\JGB\重庆\主城区（重庆）_XXJ_2022_05月公路.zip")
 # 描述Notepad.exe进程内的窗口
 
 dlg_spec = app.UntitledNotepad
@@ -18,6 +18,7 @@ dlg = app.Dialog
 type= dlg.child_window(title="攻击类型", auto_id="920", control_type="ComboBox")
 
 type.打开.click()
+
 
 # type.print_control_identifiers()
 xz=type.child_window(title="攻击类型", control_type="List")
@@ -36,16 +37,31 @@ while (count <= 0):
     dlg.child_window(title="停止", auto_id="903", control_type="Pane").click_input()
   else:
     print(0)
-send_keys('{y}')
-dlg.print_control_identifiers()
+    
+send_keys('{VK_TAB}')
+send_keys('{ENTER}')
+time.sleep(1)
+send_keys('{ENTER}')
+# dlg.print_control_identifiers()
 
-a1=dlg.child_window(title="加密密钥已成功恢复!", control_type="Window")
-a2=a1.child_window(title="确定", auto_id="1101", control_type="Pane")
-a3=a2.click_input()
+# a1=dlg.child_window(title="加密密钥已成功恢复!", control_type="Window")
+# a2=a1.child_window(title="确定", auto_id="1101", control_type="Pane")
+# a3=a2.click_input()
+# send_keys('{ENTER}')
+# time.sleep(3)
+# send_keys('{y}')
+# time.sleep(3)
+time.sleep(1)
 send_keys('{ENTER}')
 
-send_keys('{y}')
+time.sleep(1)
+send_keys('{ENTER}')
 
+time.sleep(1)
+dlg.close()
+time.sleep(1)
+send_keys('{VK_TAB}')
+send_keys('{ENTER}')
 # timer0=threading.Timer(2,send_keys('{ENTER}'))  # 每秒运行
 # timer0.start()  # 执行方法
 
